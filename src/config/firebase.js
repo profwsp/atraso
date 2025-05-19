33ca97f8201c2f4e0e96ee741cfb514dcb30d081
Washington Paiva <124314738+profwsp@users.noreply.github.com>
2025-05-19 13:43:50 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,13 +9,7 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 // Configuração do Firebase
 // NOTA: Em um ambiente de produção, estas informações devem ser armazenadas em variáveis de ambiente
 const firebaseConfig = {
-  apiKey: "AIzaSyBZhaZFLXo93QcB6MOXhTkkAsnSsYyPTis",
-  authDomain: "suzano-124a4.firebaseapp.com",
-  projectId: "suzano-124a4",
-  storageBucket: "suzano-124a4.appspot.com",
-  messagingSenderId: "162112412626",
-  appId: "1:162112412626:web:d41b30fdd68853df61460b",
-  measurementId: "G-NHNGMJ3M8K"
+ 
 };
 
 // Inicializar o Firebase
